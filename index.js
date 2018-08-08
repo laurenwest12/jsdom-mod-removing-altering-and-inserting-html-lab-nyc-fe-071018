3aa0c694463a055980a7ea78d0399477e490b434
Lauren West <laurenwest12@gmail.com>
2018-08-08 23:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
   main.remove();
   
 /* Create your new element here and assign it to newHeader */
-const newHeader = document.createElement('h1')
-newHeader.setAttribute("id","")
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
